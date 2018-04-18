fdbdc3644b6d751a9414505a6455861c45aa42b3
AtomCrafty <atomcrafty@frucost.net>
2018-04-18 12:06:34 +0200

    Separate file for FormatMapper

--- src/Yuka.Gui/Configuration/Config.cs
+++ src/Yuka.Gui/Configuration/Config.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Windows;
 using PropertyChanged;
-using Yuka.IO;
 
 namespace Yuka.Gui.Configuration {
 	[AddINotifyPropertyChangedInterface]
@@ -36,34 +33,5 @@ namespace Yuka.Gui.Configuration {
 
 		public FormatMapper ExportFormatMapping { get; set; } = new FormatMapper();
 		public FormatMapper ImportFormatMapping { get; set; } = new FormatMapper();
-	}
-
-	public sealed class FormatMapper : ObservableCollection<FormatMapping> {
-		public FormatMapper() {
-			foreach(var format in Format.RegisteredFormats) {
-				Add(new FormatMapping(format, format));
-			}
-		}
-
-		public void SetMappedFormat(Format from, Format to) {
-			this.First(m => m.From == from).To = to;
-		}
-
-		public Format GetMappedFormat(Format from) {
-			return this.First(m => m.From == from).To;
-		}
-	}
-
-	[AddINotifyPropertyChangedInterface]
-	public sealed class FormatMapping {
-		public FormatMapping(Format from, Format to = null) {
-			From = from;
-			To = to ?? from;
-		}
-
-		public Format From { get; }
-		public Format To { get; set; }
-
-		public override string ToString() => $"{From.Name} => {To.Name}";
 	}
 }
---
+++ src/Yuka.Gui/Configuration/FormatMapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using PropertyChanged;
+using Yuka.IO;
+
+namespace Yuka.Gui.Configuration {
+	public sealed class FormatMapper : ObservableCollection<FormatMapping> {
+		public FormatMapper() {
+			foreach(var format in Format.RegisteredFormats) {
+				Add(new FormatMapping(format, format));
+			}
+		}
+
+		public void SetMappedFormat(Format from, Format to) {
+			this.First(m => m.From == from).To = to;
+		}
+
+		public Format GetMappedFormat(Format from) {
+			return this.First(m => m.From == from).To;
+		}
+	}
+
+	[AddINotifyPropertyChangedInterface]
+	public sealed class FormatMapping {
+		public FormatMapping(Format from, Format to = null, string path = null) {
+			From = from;
+			To = to ?? from;
+			Path = path;
+		}
+
+		public string Path { get; set; }
+		public Format From { get; set; }
+		public Format To { get; set; }
+
+		public override string ToString() => (Path != null ? $"({Path}) " : "") + $"{From.Name} => {To.Name}";
+	}
+}
